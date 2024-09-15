c11811c6e3a3f92bba759b27c7fb310690d78ee1
Sergio Dominguez Castillo <sergio.castle@gmail.com>
2024-09-15 00:58:43 -0400

    webview activity, se crea toolbar y area de web view como cliente, es decir la pagina abrira en nuestrs aplcacion y no en un navegador

--- app/src/main/java/cl/bootcamp/sprintfinalm4/webviewActivity.java
+++ app/src/main/java/cl/bootcamp/sprintfinalm4/webviewActivity.java
@@ -1,17 +1,60 @@
 package cl.bootcamp.sprintfinalm4;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import cl.bootcamp.sprintfinalm4.databinding.ActivityWebviewBinding;
+
 
 public class webviewActivity extends AppCompatActivity {
+
+    ActivityWebviewBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
+        binding = ActivityWebviewBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        // web view
+        binding.wvContainer.getSettings().setJavaScriptEnabled(true);
+
+        binding.wvContainer.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView web, WebResourceRequest request) {
+                return false;
+            }
+        });
+
+        binding.wvContainer.loadUrl("https://www.youtube.com/watch?v=JK-Rws_pB4w");
+
+
+
+        // toolbar
+        setSupportActionBar(binding.toolbar);
+        // boton de retroceso de la toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
