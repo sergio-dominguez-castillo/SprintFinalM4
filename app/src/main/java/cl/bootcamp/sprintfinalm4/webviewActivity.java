0c73ef41ff1215dadd5f2ae6478645f9aef0228b
Sergio Dominguez Castillo <sergio.castle@gmail.com>
2024-09-14 23:23:24 -0400

    activity webview, se crea la toolbar en xml

---
+++ app/src/main/java/cl/bootcamp/sprintfinalm4/webviewActivity.java
@@ -0,0 +1,17 @@
+package cl.bootcamp.sprintfinalm4;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class webviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+    }
+}
