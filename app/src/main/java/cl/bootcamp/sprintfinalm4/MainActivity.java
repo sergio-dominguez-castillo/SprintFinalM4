c221f6c182b641e5ebc18f3cfd7e01578951175b
Sergio Dominguez Castillo <sergio.castle@gmail.com>
2024-09-14 23:43:32 -0400

    main activity principal, se crea binding y el intent del boton para cofigurar la navegacion del boton a la WebView

--- app/src/main/java/cl/bootcamp/sprintfinalm4/MainActivity.java
+++ app/src/main/java/cl/bootcamp/sprintfinalm4/MainActivity.java
@@ -1,17 +1,37 @@
 package cl.bootcamp.sprintfinalm4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import cl.bootcamp.sprintfinalm4.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(view.getContext(), webviewActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
 
     }
 }
